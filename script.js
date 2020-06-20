d64978c5cd83507a10c3e03737972667934144cf
Sergey <tapok174@bk.ru>
2020-06-21 00:32:57 +0500

    3 commit

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-let json = 10;
+let numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?")
+
+let personalMovieDb = {
+    count: numberOfFilms,
+    movies: {},
+    actor: {},
+    genres: [],
+    privat: false
+};
+
+let a = prompt("Один из последних просмотренных фильмов?")
+let b = prompt("На сколько оцените его?")
+let c = prompt("Один из последних просмотренных фильмов?")
+let d = prompt("На сколько оцените его?")
+
+personalMovieDb.movies[a] = b;
+personalMovieDb.movies[c] = d;
+
+console.log(numberOfFilms);
+console.log(personalMovieDb);
